12de481aa58366afc6ee3dd012c97c1bbf78af96
Tamyres <tamyresfmelo@gmail.com>
2022-04-30 11:09:31 -0300

    criei get e set do encontrista

--- src/main/java/ejc/entidades/Encontrista.java
+++ src/main/java/ejc/entidades/Encontrista.java
@@ -18,4 +18,60 @@ public class Encontrista {
 	
 	private Endereco endereco;
 
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getUrlFoto() {
+		return urlFoto;
+	}
+
+	public void setUrlFoto(String urlFoto) {
+		this.urlFoto = urlFoto;
+	}
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getNomeResponsavel() {
+		return nomeResponsavel;
+	}
+
+	public void setNomeResponsavel(String nomeResponsavel) {
+		this.nomeResponsavel = nomeResponsavel;
+	}
+
+	public String getTelefoneResponsavel() {
+		return telefoneResponsavel;
+	}
+
+	public void setTelefoneResponsavel(String telefoneResponsavel) {
+		this.telefoneResponsavel = telefoneResponsavel;
+	}
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
 }
