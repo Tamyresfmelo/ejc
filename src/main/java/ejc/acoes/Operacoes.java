1189f806c419af8ee44c6535bd663d1129c2e986
kcpo <kaio.oliveira@quintoandar.com.br>
2022-04-30 12:13:03 -0300

    criar a operacao de criacao de grupo

--- src/main/java/ejc/acoes/Operacoes.java
+++ src/main/java/ejc/acoes/Operacoes.java
@@ -1,5 +1,29 @@
 package ejc.acoes;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import ejc.entidades.Encontrista;
+
 public class Operacoes {
+	
+	
+	public static Map<String,List<Encontrista>> separarGrupo(List<Encontrista> encontristaList){
+		
+		Map<String,List<Encontrista>> grupos = new HashMap<String, List<Encontrista>>();
+		
+		for (Encontrista encontrista : encontristaList) {
+			if(grupos.containsKey(encontrista.getEndereco().getBairro())){
+				grupos.get(encontrista.getEndereco().getBairro()).add(encontrista);
+			}else {
+				grupos.put(encontrista.getEndereco().getBairro(), new ArrayList<Encontrista>());
+				grupos.get(encontrista.getEndereco().getBairro()).add(encontrista);
+			}
+			
+		}
+		return grupos;
+	}
 
 }
--- src/main/java/ejc/entidades/Endereco.java
+++ src/main/java/ejc/entidades/Endereco.java
@@ -14,4 +14,54 @@ public class Endereco {
 	
 	private int cep;
 
+	public String getRua() {
+		return rua;
+	}
+
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public int getCep() {
+		return cep;
+	}
+
+	public void setCep(int cep) {
+		this.cep = cep;
+	}
+
+	
+	
 }
